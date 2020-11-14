fef0b361c2e96df4f271b1444eec9934c8a4f260
Andreww02 <wirahadi.andrew@gmail.com>
2020-11-14 13:08:35 +0700

    week10

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import{
-    create
+    index,
+    create,
+    remove,
+    update
 } from './user.controller';
 
 let userRouter = express.Router();
 
+userRouter.get('/', index);
 userRouter.post('/', create);
+userRouter.delete('/:id', remove);
+userRouter.put('/:id', update);
 
 export default userRouter;
